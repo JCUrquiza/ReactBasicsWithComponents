b49afbad9174503065f51a1f676ae48e7014a195
Juan <gurquiza8@gmail.com>
2023-05-11 13:37:31 -0500

    pasando props de hijo a padre padre a hijo

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,28 @@
+import { useState } from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 const Expenses = (props) => {
 
+    const [filteredYear, setFilteredYear] = useState('2022');
+
+    const filterChangeHandler = selectedYear => {
+        setFilteredYear(selectedYear);
+    }
+
     return (
-        <Card className="expenses">
-            {
-                props.expenses.map((expense) => (
-                    <ExpenseItem key={ expense.id } data={ expense } />
-                ))
-            }
-        </Card>
+        <div>
+            <Card className="expenses">
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                {
+                    props.expenses.map((expense) => (
+                        <ExpenseItem key={ expense.id } data={ expense } />
+                    ))
+                }
+            </Card>
+        </div>
     )
 
 }
