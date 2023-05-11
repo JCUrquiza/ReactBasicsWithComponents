7f59ae8d7718369b523766e61be8560ac2df31ac
Juan <gurquiza8@gmail.com>
2023-05-11 17:39:13 -0500

    Separando components

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,23 +7,19 @@ const ExpenseItem =({ data }) => {
 
     const [title, setTitle] = useState(data.title);
 
-    const clickHandler = () => {
-        setTitle('Updated');
-    }
-
     return (
-        <Card className='expense-item'>
+        <li>
+            <Card className='expense-item'>
 
-            <ExpenseDate date={data.date} />
+                <ExpenseDate date={data.date} />
 
-            <div className='expense-item__description'>
-                <h2>{ title }</h2>
-                <div className='expense-item__price'>${ data.amount }</div>
-            </div>
+                <div className='expense-item__description'>
+                    <h2>{ title }</h2>
+                    <div className='expense-item__price'>${ data.amount }</div>
+                </div>
 
-            <button onClick={ clickHandler } >Change Title</button>
-
-        </Card>
+            </Card>
+        </li>
     );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
 
@@ -10,17 +11,18 @@ const Expenses = (props) => {
 
     const filterChangeHandler = selectedYear => {
         setFilteredYear(selectedYear);
-    }
+    };
+
+    const filteredExpenses = props.expenses.filter(expense => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
 
     return (
         <div>
             <Card className="expenses">
                 <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-                {
-                    props.expenses.map((expense) => (
-                        <ExpenseItem key={ expense.id } data={ expense } />
-                    ))
-                }
+
+                <ExpensesList items={filteredExpenses} />               
             </Card>
         </div>
     )
